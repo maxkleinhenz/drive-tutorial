33f2a3afa053821d67edf5d5240a85f5704299ac
Max Kleinhenz <dev@max-kleinhenz.de>
2025-03-02 19:53:14 +0100

    cleanup with queries object

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,31 +1,37 @@
+import "server-only";
+
 import { db } from "~/server/db";
 import { files_table, folders_table } from "~/server/db/schema";
 import { eq } from "drizzle-orm";
 
-export async function getAllParentsForFolder(folderId: number) {
-  const parents = [];
-  let currentFolderId: number | null = folderId;
+export const QUERIES = {
+  getAllParentsForFolder: async function (folderId: number) {
+    const parents = [];
+    let currentFolderId: number | null = folderId;
 
-  while (currentFolderId !== null) {
-    const folder = await db
-      .selectDistinct()
+    while (currentFolderId !== null) {
+      const folder = await db
+        .selectDistinct()
+        .from(folders_table)
+        .where(eq(folders_table.id, currentFolderId));
+
+      if (!folder[0]) throw new Error("Parent folder not found");
+      parents.push(folder[0]);
+      currentFolderId = folder[0]?.parent;
+    }
+
+    return parents.reverse();
+  },
+  getFiles: async function (folderId: number) {
+    return db
+      .select()
+      .from(files_table)
+      .where(eq(files_table.parent, folderId));
+  },
+  getFolders: async function (folderId: number) {
+    return db
+      .select()
       .from(folders_table)
-      .where(eq(folders_table.id, currentFolderId));
-
-    if (!folder[0]) throw new Error("Parent folder not found");
-    parents.push(folder[0]);
-    currentFolderId = folder[0]?.parent;
-  }
-
-  return parents.reverse();
-}
-
-export async function getFiles(folderId: number) {
-  return db.select().from(files_table).where(eq(files_table.parent, folderId));
-}
-export async function getFolders(folderId: number) {
-  return db
-    .select()
-    .from(folders_table)
-    .where(eq(folders_table.parent, folderId));
-}
+      .where(eq(folders_table.parent, folderId));
+  },
+};
