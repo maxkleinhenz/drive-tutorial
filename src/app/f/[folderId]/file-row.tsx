28d547cc2b8b531c97881ff82c4db667683a0707
Max Kleinhenz <dev@max-kleinhenz.de>
2025-03-08 23:18:31 +0100

    delete files

--- src/app/f/[folderId]/file-row.tsx
+++ src/app/f/[folderId]/file-row.tsx
@@ -1,5 +1,7 @@
-import { FileIcon, Folder as FolderIcon } from "lucide-react";
+import { FileIcon, Folder as FolderIcon, Trash2Icon } from "lucide-react";
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import { deleteFile } from "~/server/actions";
 import type { files_table, folders_table } from "~/server/db/schema";
 
 export function FileRow(props: { file: typeof files_table.$inferSelect }) {
@@ -20,8 +22,18 @@ export function FileRow(props: { file: typeof files_table.$inferSelect }) {
             {file.name}
           </a>
         </div>
-        <div className="col-span-3 text-gray-400">{"file"}</div>
+
+        <div className="col-span-2 text-gray-400">{"file"}</div>
         <div className="col-span-3 text-gray-400">{file.size}</div>
+        <div className="col-span-1 text-gray-400">
+          <Button
+            variant="ghost"
+            onClick={() => deleteFile(file.id)}
+            aria-label="Delete file"
+          >
+            <Trash2Icon size={16} />
+          </Button>
+        </div>
       </div>
     </li>
   );
@@ -46,8 +58,9 @@ export function FolderRow(props: {
             {folder.name}
           </Link>
         </div>
+        <div className="col-span-2 text-gray-400"></div>
         <div className="col-span-3 text-gray-400"></div>
-        <div className="col-span-3 text-gray-400"></div>
+        <div className="col-span-1 text-gray-400"></div>
       </div>
     </li>
   );
